f25902fbd2e325674f7b8b948d7422ecfb386db9
charivna <124678194+charivna@users.noreply.github.com>
2023-09-07 10:07:08 +0200

    hooks app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,111 +9,87 @@ import { Modal } from './Modal/Modal';
 import { toast } from 'react-toastify';
 
 import { ColorRing } from 'react-loader-spinner';
-export class App extends Component {
-  state = {
-    error: false,
-    inputValue: '',
-    images: [],
-    totalImg: null,
-    page: 1,
-    loading: false,
-    modalImg: '',
-    modalAlt: '',
-    showModal: false,
+
+export const App = () => {
+  const [error, setError] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [images, setImages] = useState([]);
+  const [totalImg, setTotalImg] = useState(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [modalImg, setModalImg] = useState('');
+  const [modalAlt, setModalAlt] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSearchSubmit = inputValue => {
+    setInputValue(inputValue);
+    setImages([]);
+    setPage(1);
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  handleSearchSubmit = inputValue => {
-    this.setState({
-      inputValue,
-      images: [],
-      page: 1,
-    });
-  };
-
-  handleClickImg = evt => {
+  const handleClickImg = evt => {
     const imgForModal = evt.target.dataset.src;
     const altForModal = evt.target.alt;
-    this.setState({
-      showModal: true,
-      modalImg: imgForModal,
-      modalAlt: altForModal,
-    });
+
+    setShowModal(true);
+    setModalImg(imgForModal);
+    setModalAlt(altForModal);
   };
 
-  closeModal = () => {
-    this.setState({
-      showModal: false,
-    });
+  const closeModal = () => {
+    setShowModal(false);
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      this.state.page !== prevState.page ||
-      this.state.inputValue !== prevState.inputValue
-    ) {
-      this.setState({ loading: true });
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
-      fetchImages(this.state.inputValue, this.state.page)
-        .then(({ hits, totalHits }) => {
-          if (!hits.length) {
-            toast.error(`No pictures with "${this.state.inputValue}"`);
-            return;
-          }
+  useEffect(() => {
+    if (!inputValue) return;
+    setLoading(true);
 
-          this.setState(prevState => ({
-            images: [...prevState.images, ...hits],
-            totalImg: totalHits,
-          }));
-        })
-        .catch(error => this.setState({ error: true }))
-        .finally(() => this.setState({ loading: false }));
-    }
-  }
+    fetchImages(inputValue, page)
+      .then(({ hits, totalHits }) => {
+        if (!hits.length) {
+          toast.error(`No pictures with "${inputValue}"`);
+          return;
+        } else {
+          setImages(state => [...state, ...hits]);
+          setTotalImg(totalHits);
+        }
+      })
+      .catch(error => setError(true))
+      .finally(() => setLoading(false));
+  }, [page, inputValue]);
 
-  render() {
-    const { images, loading, error, showModal, page, totalImg } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSearchSubmit} />
-        {error && !loading && <h1>Something wrong. Reload the page!</h1>}
-        {loading && (
-          <ColorRing
-            visible={true}
-            height="80"
-            width="80"
-            ariaLabel="blocks-loading"
-            wrapperStyle={{}}
-            wrapperClass="blocks-wrapper"
-            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-          />
-        )}
+  return (
+    <>
+      <Searchbar onSubmit={handleSearchSubmit} />
+      {error && !loading && <h1>Something wrong. Reload the page!</h1>}
+      {loading && (
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+        />
+      )}
 
-        {this.state.images.length > 0 && (
-          <ImageGallery
-            images={this.state.images}
-            clickImg={this.handleClickImg}
-          />
-        )}
-        {images.length !== 0 && page < Math.ceil(totalImg / 12) && (
-          <Button onClick={this.handleLoadMore} />
-        )}
-        {showModal && (
-          <Modal
-            img={this.state.modalImg}
-            alt={this.state.modalAlt}
-            onCloseModal={this.closeModal}
-          >
-            Some
-          </Modal>
-        )}
-        <ToastContainer autoClose={3000} />
-      </>
-    );
-  }
-}
+      {images.length > 0 && (
+        <ImageGallery images={images} clickImg={handleClickImg} />
+      )}
+      {images.length !== 0 && page < Math.ceil(totalImg / 12) && (
+        <Button onClick={handleLoadMore} />
+      )}
+      {showModal && (
+        <Modal img={modalImg} alt={modalAlt} onCloseModal={closeModal}>
+          Some
+        </Modal>
+      )}
+      <ToastContainer autoClose={3000} />
+    </>
+  );
+};
